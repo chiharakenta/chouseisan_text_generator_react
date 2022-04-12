578fa210b1ed4f416c33b519dcafbe9880ceac63
kenta chihara <kenta.chihara@tam-bourine.co.jp>
2022-04-12 21:31:22 +0900

    feat: calendar view

--- src/App.tsx
+++ src/App.tsx
@@ -6,118 +6,27 @@ import { State } from './types/State';
 import { Schedule } from './types/Schedule';
 import { Time } from './types/Time';
 
+import { times } from './constants/times';
 class App extends Component {
   state: State = {
     schedules: null,
   };
 
   componentDidMount() {
-    const schedules = [
-      {
-        year: 2022,
-        month: 4,
-        date: 11,
-        day: '月',
-        times: [
-          {
-            time: 0,
-            active: false,
-          },
-          {
-            time: 1,
-            active: false,
-          },
-          {
-            time: 2,
-            active: false,
-          },
-          {
-            time: 3,
-            active: false,
-          },
-          {
-            time: 4,
-            active: false,
-          },
-          {
-            time: 5,
-            active: false,
-          },
-          {
-            time: 6,
-            active: false,
-          },
-          {
-            time: 7,
-            active: false,
-          },
-          {
-            time: 8,
-            active: false,
-          },
-          {
-            time: 9,
-            active: false,
-          },
-          {
-            time: 10,
-            active: false,
-          },
-          {
-            time: 11,
-            active: false,
-          },
-          {
-            time: 12,
-            active: false,
-          },
-          {
-            time: 13,
-            active: false,
-          },
-          {
-            time: 14,
-            active: false,
-          },
-          {
-            time: 15,
-            active: false,
-          },
-          {
-            time: 16,
-            active: false,
-          },
-          {
-            time: 17,
-            active: false,
-          },
-          {
-            time: 18,
-            active: false,
-          },
-          {
-            time: 19,
-            active: false,
-          },
-          {
-            time: 20,
-            active: false,
-          },
-          {
-            time: 21,
-            active: false,
-          },
-          {
-            time: 22,
-            active: false,
-          },
-          {
-            time: 23,
-            active: false,
-          },
-        ],
-      },
-    ];
+    const today: Date = new Date();
+    const daysToSunday: number = 7 - today.getDay();
+    let schedules: Array<any> = [];
+    const dayString: Array<string> = ['日', '月', '火', '水', '木', '金', '土'];
+    for (let i = 0; i <= daysToSunday; i++) {
+      schedules.push({
+        year: today.getFullYear(),
+        month: today.getMonth() + 1,
+        date: today.getDate(),
+        day: dayString[today.getDay()],
+        times: times,
+      });
+      today.setDate(today.getDate() + 1);
+    }
     this.setState({
       schedules: schedules,
     });
@@ -126,11 +35,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello World</h1>
         <div className="d-flex">
           {this.state.schedules?.map((schedule: Schedule) => {
             return (
-              <div className="schedule d-flex flex-column">
+              <div className="schedule d-flex flex-column px-1 border-start border-end">
                 <h5 className="date-text">
                   {schedule.month}/{schedule.date}({schedule.day})
                 </h5>
